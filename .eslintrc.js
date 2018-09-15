486f064316492946ad5373698d095bf7b3eb5065
Hasan Wajahat <hasan_wajahat@hotmail.com>
2018-09-15 22:58:32 +0500

    Adds eslint and js beautify options

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+      "react/jsx-filename-extension": "off"
+    },
+    "globals": {
+      "window": true,
+      "document": true
+    }
+};
